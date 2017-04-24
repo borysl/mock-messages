b18bb054fa479e5b6f6de576524ac634013b1cb7
blebeda <blebeda@playtika.com>
2017-04-24 13:39:21 +0300

    Implement tests

--- tests/postRequests.js
+++ tests/postRequests.js
@@ -1,29 +1,74 @@
 var chai = require('chai');
+var request = require('request');
 var expect = chai.expect; // we are using the "expect" style of Chai
 
 var host = 'localhost';
 var port = 8080;
 
-describe('messaging webservice', function() {
-  it('POST /userMessages should put message to a specific user', function(done) {
-    var request = require('request');
+function sendPostRequest(requestUrl, requestData, callback) {
     // Set the headers
     var headers = {
-        'User-Agent':       'Super Agent/0.0.1',
-        'Content-Type':     'application/x-www-form-urlencoded'
+        'Content-Type':     'application/json'
     }
 
     // Configure the request
     var options = {
-        url: `http://${host}:${port}/userMessages`,
+        url: `http://${host}:${port}/${requestUrl}`,
         method: 'POST',
-        headers: headers
+        json: true,
+        headers: headers,
+        body: requestData
     }
 
     request.post(options, function (err, httpResponse, body) {
         console.log(err, body);
+        callback(err);
+    });
+}
+
+describe('messaging webservice', function() {
+  it('POST /userMessages should put message to a specific user', function(done) {
+    var requestUrl='userMessages';
+
+    var requestData = {
+        "messages": [
+            {
+                "userId": 1234564565454,
+                "messageId": "100500",
+                "payload": "someInternalStuff",
+                "createdTs": 123,
+                "expireTs": 12345
+            }
+        ]
+    };
+
+    sendPostRequest(requestUrl, requestData, function(err) {
         expect(err).to.equal(null);
         done();
     });
   });
+
+it('POST /retrieveMessages should retrieve message to a specific user', function(done) {
+    sendPostRequest('userMessages', {
+        "messages": [
+            {
+                "userId": 234123215342,
+                "messageId": "42",
+                "payload": "someInternalStuff",
+                "createdTs": 123,
+                "expireTs": 12345
+            }
+        ]
+    }, function(err) {
+        expect(err).to.equal(null);
+        sendPostRequest('retrieveMessages', {
+            "userId": 234123215342,
+            "sessionId": 23215342
+        }, function (err2) {
+            expect(err2).to.equal(null);
+            done();
+        });
+    });
+  });
 });
+
